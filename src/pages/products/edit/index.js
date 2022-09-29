fe4bbfac2a007c695716ead1f8d28f13ee3350ff
amozalev <mozalev.alexey@gmail.com>
2022-09-29 17:35:33 +0300

    Rendered product from on the Edit page.

--- src/pages/products/edit/index.js
+++ src/pages/products/edit/index.js
@@ -1,7 +1,22 @@
+import ProductForm from '../../../components/product-form';
+
 export default class Page {
   element;
   subElements = {};
-  components = {};
+  components = [];
+
+  async addProductForm() {
+    const strippedPathArr = decodeURI(window.location.pathname)
+      .replace(/^\/|\/$/, '')
+      .split('/');
+    const id = strippedPathArr.length ? strippedPathArr[strippedPathArr.length - 1] : '';
+
+    const form = new ProductForm(id);
+    await form.render();
+
+    this.element.append(form.element);
+    this.components.push(form);
+  }
 
   async render() {
     const element = document.createElement('div');
@@ -13,6 +28,24 @@ export default class Page {
 
     this.element = element.firstElementChild;
 
+    await this.addProductForm();
+
     return this.element;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = null;
+
+    for (const component of this.components) {
+      component.destroy();
+    }
   }
 }
